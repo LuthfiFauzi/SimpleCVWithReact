74fe9defacf7a5a0e36ee5a1d25ef11e06f1d22c
Luthfi Fauzi <faiz.uzi96@gmail.com>
2020-11-21 09:43:10 +0700

    View News Successfully
    End

--- src/pages/infoCorona/index.js
+++ src/pages/infoCorona/index.js
@@ -3,8 +3,7 @@ import { CoronaNews } from '../../components';
 
 const InfoCorona = () => {
   return (
-    <div className="pageInfoCorona-wrapper, content">
-      <h1>Info Corona</h1>
+    <div className="pageInfoCorona-wrapper">
       <CoronaNews />
     </div>
   );
